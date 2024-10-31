ca0dae87ed7bb8eeebde9d92b696f0395081ebff
Alafresh <alafresh16@gmail.com>
2024-10-31 11:23:26 -0500

    unit movement

---
+++ Assets/Code/Scripts/Unit.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 _targetPosition;
+
+    private void Update()
+    {
+        float stoppingDistance = 0.1f;
+        if (Vector3.Distance(transform.position, _targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (_targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * (moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            Move(new Vector3(4, 0, 4));
+        }
+    }
+        
+    private void Move(Vector3 targetPosition)
+    {
+        _targetPosition = targetPosition;
+    }
+}
