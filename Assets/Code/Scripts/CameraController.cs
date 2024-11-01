7b45b561887b2fc9ccdef18d26b97ceb450d1a51
Alafresh <alafresh16@gmail.com>
2024-11-01 15:06:35 -0500

    Update all

---
+++ Assets/Code/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 inputMoveDir = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputMoveDir.z += 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputMoveDir.z -= 1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputMoveDir.x -= 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputMoveDir.x += 1f;
+        }
+
+        float moveSpeed = 5f;
+        transform.position += inputMoveDir * (moveSpeed * Time.deltaTime);
+    }
+}
