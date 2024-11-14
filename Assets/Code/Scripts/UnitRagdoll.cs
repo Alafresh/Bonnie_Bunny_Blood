cbb8ce20b79e6764d191d2cee51dfe66b020487c
Alafresh <alafresh16@gmail.com>
2024-11-14 12:06:06 -0500

    add ragdoll system

---
+++ Assets/Code/Scripts/UnitRagdoll.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class UnitRagdoll : MonoBehaviour
+{
+    [SerializeField] private Transform ragdollRootBone;
+
+    public void SetUp(Transform originalRootBone)
+    {
+        MatchAllChildTransforms(originalRootBone, ragdollRootBone);
+        ApplyExplosionToRagdoll(ragdollRootBone, 300f, transform.position, 10f);
+    }
+
+    private void MatchAllChildTransforms(Transform root, Transform clone)
+    {
+        foreach (Transform child in root)
+        {
+            Transform cloneChild = clone.Find(child.name);
+            if (cloneChild != null)
+            {
+                cloneChild.position = child.position;
+                cloneChild.rotation = child.rotation;
+                MatchAllChildTransforms(child, cloneChild);
+            }
+        }
+    }
+
+    private void ApplyExplosionToRagdoll(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
+    {
+        foreach (Transform child in root)
+        {
+            if (child.TryGetComponent(out Rigidbody childRigidbody))
+            {
+                childRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            }
+            ApplyExplosionToRagdoll(child, explosionForce, explosionPosition, explosionRange);
+        }
+    }
+}
---
+++ Assets/Code/Scripts/UnitRagdollSpawner.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class UnitRagdollSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform ragdollPrefab;
+    [SerializeField] private Transform originalRootBone;
+    
+    private HealthSystem _healthSystem;
+
+    private void Awake()
+    {
+        _healthSystem = GetComponent<HealthSystem>();
+        _healthSystem.OnDead += HealthSystem_OnDead;
+    }
+
+    private void HealthSystem_OnDead(object sender, EventArgs e)
+    {
+        Transform ragdollTransform = Instantiate(ragdollPrefab, transform.position, transform.rotation);
+        UnitRagdoll unitRagdoll = ragdollTransform.GetComponent<UnitRagdoll>();
+        unitRagdoll.SetUp(originalRootBone);
+    }
+}
