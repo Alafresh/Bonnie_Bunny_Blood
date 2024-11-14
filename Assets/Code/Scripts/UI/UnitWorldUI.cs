ad0c01756b5a81f65f99d5785791326a6555301d
Alafresh <alafresh16@gmail.com>
2024-11-14 12:42:20 -0500

    add lookatcamera

--- Assets/Code/Scripts/UI/UnitWorldUI.cs
+++ Assets/Code/Scripts/UI/UnitWorldUI.cs
@@ -1,16 +1,27 @@
+using System;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class UnitWorldUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TextMeshProUGUI actionPointsText;
+    [SerializeField] private Unit unit;
+    [SerializeField] private Image healthBarImage;
+    [SerializeField] private HealthSystem healthSystem;
+
+    private void Start()
     {
-        
+        Unit.OnAnyActionsPointsChanged += Unit_OnAnyActionPointsChanged;
+        UpdateActionPointsText();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateActionPointsText()
     {
-        
+        actionPointsText.text = unit.GetActionsPoints().ToString();
+    }
+
+    private void Unit_OnAnyActionPointsChanged(object sender, System.EventArgs e)
+    {
+        UpdateActionPointsText();
     }
 }
