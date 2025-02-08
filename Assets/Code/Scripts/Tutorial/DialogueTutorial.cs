dcbd7e4663bdb54b9a1fa79002150d7bc5b0090c
Alafresh <51685896+Alafresh@users.noreply.github.com>
2025-02-08 16:45:10 -0500

    update

---
+++ Assets/Code/Scripts/Tutorial/DialogueTutorial.cs
@@ -0,0 +1,35 @@
+using Febucci.UI;
+using UnityEngine;
+
+public class DialogueTutorial : MonoBehaviour
+{
+    public TypewriterByCharacter textAnimatorPlayer;
+
+    [TextArea(3, 50), SerializeField]
+    string[] textToShow;
+    private int index;
+
+    private void Awake()
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(textAnimatorPlayer, $"Text Animator Player component is null in {gameObject.name}");
+    }
+
+    private void Start()
+    {
+        ShowText();
+    }
+
+    public void ShowText()
+    {
+        textAnimatorPlayer.ShowText(textToShow[0]);
+    }
+
+    public void NextText()
+    {
+        index++;
+        if (index < textToShow.Length)
+        {
+            textAnimatorPlayer.ShowText(textToShow[index]);
+        }
+    }
+}
