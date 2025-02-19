2c94f97f452d03b7c1ade9e3977c0a2249eb3933
Alafresh <51685896+Alafresh@users.noreply.github.com>
2025-02-19 14:48:42 -0500

    support video web

--- Assets/Code/Scripts/Tutorial/DialogueTutorial.cs
+++ Assets/Code/Scripts/Tutorial/DialogueTutorial.cs
@@ -9,6 +9,8 @@ public class DialogueTutorial : MonoBehaviour
     [SerializeField] VideoClip[] videoClips;
     [SerializeField] GameObject dialoguePanel;
     public TypewriterByCharacter textAnimatorPlayer;
+    private string[] urls;
+    [SerializeField] string[] videoNames;
 
     [TextArea(3, 50), SerializeField]
     string[] textToShow;
@@ -22,6 +24,19 @@ public class DialogueTutorial : MonoBehaviour
     private void Start()
     {
         ShowText();
+#if UNITY_WEBGL
+        SetUpVideoCLips();
+#endif
+    }
+
+    private void SetUpVideoCLips()
+    {
+        videoPlayer.source = VideoSource.Url;
+        urls = new string[videoClips.Length];
+        for (int i = 0; i < videoClips.Length; i++)
+        {
+            urls[i] = System.IO.Path.Combine(Application.streamingAssetsPath, videoNames[i]);
+        }
     }
 
     public void ShowText()
@@ -35,28 +50,52 @@ public class DialogueTutorial : MonoBehaviour
         switch (index)
         {
             case 7:
-                
+                videoRender.SetActive(true);
+#if UNITY_WEBGL
+                videoPlayer.url = urls[0];
+#else
                 videoPlayer.clip = videoClips[0];
+#endif
                 videoPlayer.Play();
                 break;
             case 8:
+#if UNITY_WEBGL
+                videoPlayer.url = urls[1];
+#else
                 videoPlayer.clip = videoClips[1];
+#endif
                 videoPlayer.Play();
                 break;
             case 9:
+#if UNITY_WEBGL
+                videoPlayer.url = urls[2];
+#else
                 videoPlayer.clip = videoClips[2];
+#endif
                 videoPlayer.Play();
                 break;
             case 10:
+#if UNITY_WEBGL
+                videoPlayer.url = urls[3];
+#else
                 videoPlayer.clip = videoClips[3];
+#endif
                 videoPlayer.Play();
                 break;
             case 11:
+#if UNITY_WEBGL
+                videoPlayer.url = urls[4];
+#else
                 videoPlayer.clip = videoClips[4];
+#endif
                 videoPlayer.Play();
                 break;
             case 12:
+#if UNITY_WEBGL
+                videoPlayer.url = urls[5];
+#else
                 videoPlayer.clip = videoClips[5];
+#endif
                 videoPlayer.Play();
                 break;
             case 13:
